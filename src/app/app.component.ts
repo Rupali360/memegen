62c6508d523afabfc4759838c55c293da04e47d0
Rupali360 <137111195+Rupali360@users.noreply.github.com>
2025-03-12 10:04:23 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,14 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'memegen';
+  lable1 = '';
+  lable2 = '';
+
+  change() {
+    
+  }
 }
