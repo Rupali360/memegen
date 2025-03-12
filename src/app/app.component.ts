42ae0c5a36d2175e55e42356b56da55848645e10
Rupali360 <137111195+Rupali360@users.noreply.github.com>
2025-03-12 10:32:55 +0530

    abc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,11 @@ export class AppComponent {
   title = 'memegen';
   lable1 = '';
   lable2 = '';
-
+  lable1img ='';
+  lable2img ='';
   change() {
-    
+   this.lable1img = this.lable1;
+   this.lable2img = this.lable2;
+   
   }
 }
